2495f607141a10c2a9611c3816a4ce80b8ffba31
wyz23x2 <wyz23x2@163.com>
2022-01-31 17:18:03 +0800

    Add option `--ld`

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ if __name__ == '__main__':
     parser.add_argument('-V', '--version', action='version', version=VERSION)
     if DEBUG < 2:
         parser.add_argument('-d', '--debug', action='store_true', help='debug mode, DEBUG = 1')
+        parser.add_argument('--ld', '--lower-debug', action='count', help='ignored in normal builds')
     else:
         parser.add_argument('-d', '--debug', action='store_true', help='ignored in debug builds')
+        parser.add_argument('--ld', '--lower-debug', action='count', help='DEBUG = 1; if specified twice, DEBUG = 0')
     ns = parser.parse_args()
+    if DEBUG < 2 and ns.debug:
+        DEBUG = 1
+    elif ns.ld:
+        DEBUG = max(2-ns.ld, 0)
