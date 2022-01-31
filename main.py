7c039357d676310e2379b259537a1c5d7941f335
wyz23x2 <wyz23x2@163.com>
2022-01-31 16:12:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-DEBUG = 2
+DEBUG   = 2
+VERSION = '0.1.0a1'
 if __name__ == '__main__':
     import argparse as ap
     parser = ap.ArgumentParser('XBC', allow_abbrev=False)
+    parser.add_argument('-V', '--version', action='version', version=VERSION)
+    if DEBUG < 2:
+        parser.add_argument('-d', '--debug', action='store_true', help='debug mode, DEBUG = 1')
+    else:
+        parser.add_argument('-d', '--debug', action='store_true', help='ignored in debug builds')
+    ns = parser.parse_args()
