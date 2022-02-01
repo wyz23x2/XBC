7b85146ebc2f9570e9e051acd518dc74f4bf19d3
wyz23x2 <wyz23x2@163.com>
2022-02-01 17:19:52 +0800

    Add Lib

---
+++ Lib/_threading.d
@@ -0,0 +1,15 @@
+module threading;
+
+// import pyd.pyd;
+import std.stdio;
+import std.bigint;
+import std.concurrency;
+
+
+
+extern(C) @disable void PydMain() {
+    version (none){
+        def!(/*...*/)(/*...*/);
+    }
+    // module_init();
+}
