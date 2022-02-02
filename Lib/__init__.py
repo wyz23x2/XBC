a23f7aae8ecde580e2c08156302234adc1deadd6
wyz23x2 <wyz23x2@163.com>
2022-02-02 14:29:28 +0800

    Link `__init__.py` to `ifix.py`

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -1,7 +1,1 @@
-import os
-import sys
-d = os.path.dirname(os.path.realpath(__file__))
-if '\\' in d:
-    sys.path.append('\\'.join(d.split('\\')[:-1]))
-else:
-    sys.path.append('/'.join(d.split('/')[:-1]))
+ifix.py
