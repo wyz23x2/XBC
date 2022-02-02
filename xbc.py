710ed3af8b7c449acb9269af38e35acab2e78291
wyz23x2 <wyz23x2@163.com>
2022-02-02 12:10:20 +0800

    Move `VERSION` to xbc.py & add `VERHEX`

--- xbc.py
+++ xbc.py
@@ -1,1 +1,8 @@
+VERSION = '0.1.0a1'
+#          Minor Level (dev->0, a->1, b->2, c->3, final->4)
+#             vv  v
+VERHEX  = 0o00010011
+#           ^^  ^^ ^ Serial
+#        Major Micro
 from utils import *
+print(VERHEX)
